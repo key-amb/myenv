d864cb4773ba3af6fc00a687aae8fab7e6e65434
key-amb <yasutake.kiyoshi@gmail.com>
2016-06-06 00:44:00 +0900

    [ubuntu-desktop] Add script to install extra pkgs

---
+++ envs/ubuntu-desktop/script/install-extra-pkgs.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BASE_DIR=$HOME/.myenv
+. $BASE_DIR/lib/package/dpkg.bashrc
+
+install_pkg slack-desktop
+
+exit
+
+: <<'__EOF__'
+
+=encoding utf8
+
+=head1 NAME
+
+B<install-extra-pkgs> - 
+
+=head1 SYNOPSYS
+
+    install-extra-pkgs
+
+=head1 DESCRIPTION
+
+blah blah blah.
+
+=head1 AUTHORS
+
+YASUTAKE Kiyoshi E<lt>yasutake.kiyoshi@gmail.comE<gt>
+
+=cut
+
+__EOF__
