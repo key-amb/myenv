1fd84b780379b702c94108d8362c2667051d2017
key-amb <yasutake.kiyoshi@gmail.com>
2016-07-03 08:19:58 +0900

    Add script to install Golang programs

---
+++ script/install-go-binaries.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+base_dir="$(cd $(dirname $0)/..; pwd)"
+. $base_dir/lib/setup.bashrc
+
+GO_PACKAGES=(
+  "peco:0.3.6"
+  "hub:2.2.3"
+)
+
+cd $BASE_DIR/submodule/install-github-go-binary
+for gopkg in ${GO_PACKAGES[@]}; do
+  _ifs=$IFS
+  IFS=:
+  set -- $gopkg
+  sudo bin/install-github-go-binary $1 $2
+  IFS=$_ifs
+done
+
+exit
+
+: <<'__EOF__'
+
+=encoding utf8
+
+=head1 NAME
+
+B<install-go-binaries.sh> - 
+
+=head1 SYNOPSYS
+
+    install-go-binaries.sh
+
+=head1 DESCRIPTION
+
+blah blah blah.
+
+=head1 AUTHORS
+
+IKEDA Kiyoshi E<lt>yasutake.kiyoshi@gmail.comE<gt>
+
+=cut
+
+__EOF__
+
